3c84530955155fa41375e26a6e47415ba32a0890
Lily Lindfors <lilylindfors@Lilys-MacBook-Air.local>
2021-09-02 14:58:02 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int n = 1;
+        boolean found = false;
+
+        while (!found) {
+            int value = n*n*n - 1000*n*n;
+            if (value > 1000000);
+            {
+                found = true;
+                System.out.println ("n; " + n);
+                System.out.println ("expr: " + value):
+            }
+            
+
+        }
+
+
+
+
+    }
+}
